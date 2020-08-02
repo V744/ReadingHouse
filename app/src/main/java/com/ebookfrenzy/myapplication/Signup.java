afb8fcfc4faabe29eefb1e7c136b68001d4b84d4
sim399 <honeykhaira416@gmail.com>
2020-08-02 17:57:54 -0400

    Sign up is done

---
+++ app/src/main/java/com/ebookfrenzy/myapplication/Signup.java
@@ -0,0 +1,18 @@
+package com.ebookfrenzy.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        {
+
+        }
+    }
+}
