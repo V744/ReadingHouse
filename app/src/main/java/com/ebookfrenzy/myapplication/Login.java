713c9060b983ba7717fb29443708cf43f8909cd3
sahilsaini78 <sahil7.saini7@gmail.com>
2020-08-02 19:47:58 -0400

    connecting login page to signup and forgot password

--- app/src/main/java/com/ebookfrenzy/myapplication/Login.java
+++ app/src/main/java/com/ebookfrenzy/myapplication/Login.java
@@ -2,13 +2,47 @@ package com.ebookfrenzy.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+    Button login,forgot,sign;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+      login=  findViewById(R.id.login);
+        forgot=findViewById(R.id.forgot);
+       sign= findViewById(R.id.sign);
+
+       login.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent i= new Intent(Login.this,HomeActivity.class);
+               startActivity(i);
+           }
+       });
+
+       forgot.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent i= new Intent(Login.this,ForgotPass.class);
+               startActivity(i);
+           }
+       });
+        sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(Login.this,Signup.class);
+                startActivity(i);
+            }
+        });
+
+
+
     }
 }
