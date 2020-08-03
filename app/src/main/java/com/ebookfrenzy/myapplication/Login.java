4844ddaabf63b12c322cd8838fa385a82d4b50fa
V744 <vivekmalhotra676@gmail.com>
2020-08-02 20:10:30 -0400

    Connecting Screen With Login Page

--- app/src/main/java/com/ebookfrenzy/myapplication/Login.java
+++ app/src/main/java/com/ebookfrenzy/myapplication/Login.java
@@ -2,13 +2,60 @@ package com.ebookfrenzy.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    public Button b1, b2, b3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        b1 = (Button) findViewById(R.id.button3);
+        b2 = (Button) findViewById(R.id.button5);
+        b3 = (Button) findViewById(R.id.button4);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openHomeActivity();
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openForgotPass();
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSignup();
+            }
+        });
     }
+
+    public void openHomeActivity() {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
+
+    public void openForgotPass() {
+        Intent intent = new Intent(this, ForgotPass.class);
+        startActivity(intent);
+    }
+
+    public void openSignup() {
+        Intent intent = new Intent(this, Signup.class);
+        startActivity(intent);
+    }
+
+
+
+
+
 }
