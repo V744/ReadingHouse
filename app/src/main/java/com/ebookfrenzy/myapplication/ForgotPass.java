bdb6a8300ed6c8c112e2eba2d89a4bf8eaa42155
sahilsaini78 <sahil7.saini7@gmail.com>
2020-08-04 12:59:04 -0400

    LInk ForgotPass to login

--- app/src/main/java/com/ebookfrenzy/myapplication/ForgotPass.java
+++ app/src/main/java/com/ebookfrenzy/myapplication/ForgotPass.java
@@ -2,13 +2,27 @@ package com.ebookfrenzy.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 
 public class ForgotPass extends AppCompatActivity {
-
+Button save;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_pass);
-    }
-}
+        save=  findViewById(R.id.save);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ForgotPass.this, Login.class);
+
+                startActivity(i);
+            }
+
+    });
+}}
