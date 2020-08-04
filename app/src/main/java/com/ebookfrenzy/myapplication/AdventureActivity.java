d2c1323c71d499e56ddfd9d2ff4b6155b578e754
sushmithavootla <sushmithavootla2@gmail.com>
2020-08-04 17:05:57 -0400

    set onclick in cardview ,list view added

--- app/src/main/java/com/ebookfrenzy/myapplication/AdventureActivity.java
+++ app/src/main/java/com/ebookfrenzy/myapplication/AdventureActivity.java
@@ -3,12 +3,27 @@ package com.ebookfrenzy.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class AdventureActivity extends AppCompatActivity {
+    private ListView listView;
+    ArrayList<User> users;
 
+    public ArrayList<User> getUsers(){
+        ArrayList<User> users=new ArrayList<>();
+
+        users.add(new User("Jack London","Adventure"));
+        return users;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adventure);
+        listView=findViewById(R.id.list_view);
+        users = getUsers();
+        AdventureAdapter adapter=new AdventureAdapter(users);
+        listView.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/ebookfrenzy/myapplication/AdventureAdapter.java
@@ -0,0 +1,60 @@
+package com.ebookfrenzy.myapplication;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class AdventureAdapter  extends BaseAdapter {
+    ArrayList<User> users;
+    LayoutInflater mInflater;
+    public AdventureAdapter(ArrayList<User> users){
+
+        this.users=users;
+    }
+
+
+    @Override
+    public int getCount() {
+        return users.size();
+    }
+
+    @Override
+    public User getItem(int position) {
+        return users.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            convertView=LayoutInflater.from(parent.getContext()).inflate(R.layout.list_view,null);
+            viewHolder=new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        }else{
+            viewHolder=(ViewHolder)convertView.getTag();
+        }
+        User user=getItem(position);
+        viewHolder.author.setText(user.getAuthor());
+        viewHolder.bookname.setText(user.getBookname());
+
+        return convertView;
+    }
+    class ViewHolder {
+        TextView author;
+        TextView bookname;
+
+        public ViewHolder(View view) {
+            author = view.findViewById(R.id.text3);
+            bookname= view.findViewById(R.id.text5);
+        }
+    }
+}
